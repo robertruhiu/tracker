0bd9fa0fbd12acc876944c2c70ddaeacaad0185e
Aurelien Giraud <aure@posteo.de>
2016-03-18 22:03:55 +0000

    Install NeDB and test that is is working

--- server.js
+++ server.js
@@ -7,12 +7,37 @@ var express = require('express'),
   app = express(),
   port = Number(process.env.PORT || 8080);
 
+// DATABASE
+// ===============================================
+
+// Setup the database.
+var Datastore = require('nedb');
+var db = new Datastore({
+  filename: 'goals.db', // provide a path to the database file 
+  autoload: true, // automatically load the database
+  timestampData: true // automatically add and manage the fields createdAt and updatedAt
+});
+
+// Let us check that we can save to the database.
+// Define a goal.
+var goal = {
+  description: 'Meditate in the morning',
+  successes: [],
+  failures: []
+};
+
+// Save this goal to the database.
+db.insert(goal, function(err, newGoal) {
+  if (err) console.log(err);
+  console.log(newGoal);
+});
+
 // ROUTES
 // ===============================================
 
 // Define the home page route.
 app.get('/', function(req, res) {
-  res.send('Our first route is working.:)');
+  res.send('Our first route is working. :)');
 });
 
 // START THE SERVER
