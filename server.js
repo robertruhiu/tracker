667f9efd143e8c02e32560bcd88b1a0cb8f1a49a
Aurelien Giraud <aure@posteo.de>
2016-03-23 22:11:05 +0000

    Add Handlebars and views using Material Desing Lite

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 // server.js
 
-// DEPENDENCIES AND SETUP
+// DEPENDENCIES
 // ===============================================
 
 var express = require('express'),
   app = express(),
-  bodyParser = require('body-parser'),
-  port = Number(process.env.PORT || 8080);
+  bodyParser = require('body-parser'), // Middleware to read POST data
+  exphbs = require('express-handlebars');
 
-// To read POST data, we use the body-parser middleware.
+// SETUP
+// ===============================================
+
+// Set the port number.
+var   port = Number(process.env.PORT || 8080);
+
+// Set up body-parser.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
+// Tell the app that the templating engine is Handlebars.
+app.engine('handlebars',
+  // Pass default configuration to express-handlebars module.
+  exphbs({
+    defaultLayout: 'main'
+  }));
+
+// Tell the app that the view engine is also Handlebars.
+app.set('view engine', 'handlebars');
 
 // DATABASE
 // ===============================================
@@ -20,16 +37,16 @@ app.use(bodyParser.urlencoded({
 // Setup the database.
 var Datastore = require('nedb');
 var db = new Datastore({
-  filename: 'goals.db', // provide a path to the database file 
-  autoload: true, // automatically load the database
-  timestampData: true // automatically add and manage the fields createdAt and updatedAt
+  filename: 'goals.db', // Provide a path to the database file.
+  autoload: true, // Automatically load the database.
+  timestampData: true // Add and manage the fields createdAt and updatedAt.
 });
 
 // ROUTES
 // ===============================================
 
-// GET all goals
-// (accessed at GET http://localhost:8080/goals)
+// GET all goals.
+// (Accessed at GET http://localhost:8080/goals)
 app.get('/goals', function(req, res) {
   db.find({}).sort({
     updatedAt: -1
@@ -39,8 +56,8 @@ app.get('/goals', function(req, res) {
   });
 });
 
-// POST a new goal
-// (accessed at POST http://localhost:8080/goals)
+// POST a new goal.
+// (Accessed at POST http://localhost:8080/goals)
 app.post('/goals', function(req, res) {
   var goal = {
     description: req.body.description,
@@ -51,8 +68,8 @@ app.post('/goals', function(req, res) {
   });
 });
 
-// DELETE a goal
-// (accessed at DELETE http://localhost:8080/goals/goal_id)
+// DELETE a goal.
+// (Accessed at DELETE http://localhost:8080/goals/goal_id)
 app.delete('/goals/:id', function(req, res) {
   var goal_id = req.params.id;
   db.remove({
@@ -63,6 +80,24 @@ app.delete('/goals/:id', function(req, res) {
   });
 });
 
+// GET the home page
+// (Accessed at GET http://localhost:8080/)
+app.get('/', function(req, res) {
+  db.find({}).sort({
+    updatedAt: -1
+  }).exec(function(err, result) {
+    if (err) console.log(err)
+    var obj = {
+      goals: result,
+      helpers: {
+        formatCreatedAt: function() {
+          return this.createdAt.toLocaleDateString()
+        }
+      }
+    }
+    res.render('index', obj)
+  })
+})
 
 // START THE SERVER
 // ===============================================
