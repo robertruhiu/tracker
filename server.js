a017ed52fcf5371c95b43e60d19ef0c60b2fcf65
Aurelien Giraud <aure@posteo.de>
2016-03-23 20:39:32 +0000

    Update routes and add post and delete functionalities.

--- server.js
+++ server.js
@@ -5,7 +5,14 @@
 
 var express = require('express'),
   app = express(),
+  bodyParser = require('body-parser'),
   port = Number(process.env.PORT || 8080);
+
+// To read POST data, we use the body-parser middleware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // DATABASE
 // ===============================================
@@ -18,25 +25,44 @@ var db = new Datastore({
   timestampData: true // automatically add and manage the fields createdAt and updatedAt
 });
 
-// Let us check that we can save to the database.
-// Define a goal.
-var goal = {
-  description: 'Do 10 minutes meditation every day',
-};
-
-// Save this goal to the database.
-db.insert(goal, function(err, newGoal) {
-  if (err) console.log(err);
-  console.log(newGoal);
-});
-
 // ROUTES
 // ===============================================
 
-// Define the home page route.
-app.get('/', function(req, res) {
-  res.send('Our first route is working. :)');
+// GET all goals
+// (accessed at GET http://localhost:8080/goals)
+app.get('/goals', function(req, res) {
+  db.find({}).sort({
+    updatedAt: -1
+  }).exec(function(err, result) {
+    if (err) console.log(err);
+    res.json(result);
+  });
 });
+
+// POST a new goal
+// (accessed at POST http://localhost:8080/goals)
+app.post('/goals', function(req, res) {
+  var goal = {
+    description: req.body.description,
+  };
+  db.insert(goal, function(err, goal) {
+    if (err) console.log(err);
+    res.redirect('/');
+  });
+});
+
+// DELETE a goal
+// (accessed at DELETE http://localhost:8080/goals/goal_id)
+app.delete('/goals/:id', function(req, res) {
+  var goal_id = req.params.id;
+  db.remove({
+    _id: goal_id
+  }, {}, function(err, goal) {
+    if (err) console.log(err);
+    res.redirect('/');
+  });
+});
+
 
 // START THE SERVER
 // ===============================================
